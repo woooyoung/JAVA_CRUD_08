9718875669cb1489b31120eb8dfe1000543c8d67
woooyoung <axdsw121@gmail.com>
2021-08-23 18:47:41 +0900

    작성된 게시물을 리스트에 저장, 목록기능

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -1,5 +1,7 @@
 package com.sbs.java.crud;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +9,8 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
 		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 			System.out.printf("명령어) ");
@@ -25,9 +29,22 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} else if (command.equals("article list")) {
-				System.out.println("게시물이 없습니다.");
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				System.out.println("번호  /  제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%d  /  %s\n", article.id, article.title);
+				}
+
 			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
 			}
@@ -36,5 +53,17 @@ public class Main {
 		sc.close();
 
 		System.out.println("==프로그램 끝==");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
