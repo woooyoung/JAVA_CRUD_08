31813352d8c6e6b21ab5e646b61d49aceaec7f93
woooyoung <axdsw121@gmail.com>
2021-08-24 18:43:06 +0900

    게시물 상세페이지, 날짜x

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -45,7 +45,32 @@ public class Main {
 					System.out.printf("%d  /  %s\n", article.id, article.title);
 				}
 
-			} else {
+			} else if (command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]); // "1" -> 1
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) { // 0 1 2 3 4
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : 2021-12-12 12:12:12\n");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+			}
+
+			else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어입니다.\n", command);
 			}
 		}
